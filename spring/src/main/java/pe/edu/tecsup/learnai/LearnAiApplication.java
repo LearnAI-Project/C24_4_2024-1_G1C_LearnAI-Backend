dc5e9eca3f7b693e35cee0ea533ae4ec3cc839a2
TheShepsMT <theshepsmt@gmail.com>
2024-12-08 01:27:14 -0500

    Add spring-dotenv

--- spring/src/main/java/pe/edu/tecsup/learnai/LearnAiApplication.java
+++ spring/src/main/java/pe/edu/tecsup/learnai/LearnAiApplication.java
@@ -1,6 +1,5 @@
 package pe.edu.tecsup.learnai;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,24 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LearnAiApplication {
 
     public static void main(String[] args) {
-        // Cargar el archivo .env
-        Dotenv dotenv = Dotenv.load();
-        // Acceder a las variables de entorno
-        String dbHost = dotenv.get("DB_URL");  // URL de la base de datos
-        String dbPort = dotenv.get("MAIL_USERNAME");  // Usuario de email
-        String dbName = dotenv.get("MAIL_PASSWORD");  // Contraseña de email
-        String dbUser = dotenv.get("GOOGLE_OAUTH_CLIENT_ID");  // ID de cliente de Google OAuth
-        String dbPassword = dotenv.get("GOOGLE_OAUTH_CLIENT_SECRET");  // Secreto de cliente de Google OAuth
-        String apiKey = dotenv.get("HOST_FRONTEND");  // URL del frontend
-
-        // Imprimir las variables para verificar
-        System.out.println("URL de base de datos: " + dbHost);
-        System.out.println("Usuario de email: " + dbPort);
-        System.out.println("Contraseña de email: " + dbName);
-        System.out.println("ID de cliente Google OAuth: " + dbUser);
-        System.out.println("Secreto cliente Google OAuth: " + dbPassword);
-        System.out.println("URL del frontend: " + apiKey);
-
         SpringApplication.run(LearnAiApplication.class, args);
     }
 
